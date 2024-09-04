70f02a426c526ef3d1afb198b629dedba2281551
Adam Lynch <adamclynch03@gmail.com>
2024-09-04 18:24:14 -0400

    fix: dates not needed

--- yfscraper/scraper.py
+++ yfscraper/scraper.py
@@ -18,14 +18,14 @@ YAHOO_LINK3 = "&period2="
 YAHOO_LINK4 = "&interval=1d&events=history&includeAdjustedClose=true"
 
 
-def _yahoo_url(ticker, start_date, end_date):
+def _yahoo_url(ticker):
     return (
         YAHOO_LINK1
         + _yahoo_ticker_format(ticker)
         + YAHOO_LINK2
-        + str(_yahoo_date_number(start_date))
+        + str(_yahoo_date_number(YAHOO_START_DATE))
         + YAHOO_LINK3
-        + str(_yahoo_date_number(end_date))
+        + str(_yahoo_date_number(datetime.datetime.today()))
         + YAHOO_LINK4
     )
 
@@ -42,21 +42,19 @@ class _Driver:
     def __del__(self):
         self._driver.quit()
 
-    def download_ticker(self, ticker, start_date, end_date, wait_time):
-        url = _yahoo_url(ticker, start_date, end_date)
+    def download_ticker(self, ticker, wait_time):
+        url = _yahoo_url(ticker)
         self._driver.get(url)
         time.sleep(wait_time)
 
 
-def _download_tickers_control(driver, stocks, wait_time):
-    for ticker in tqdm.tqdm(stocks):
-        driver.download_ticker(
-            ticker, stocks[ticker]["start_date"], stocks[ticker]["end_date"], wait_time
-        )
+def _download_tickers_control(driver, tickers, wait_time):
+    for ticker in tqdm.tqdm(tickers):
+        driver.download_ticker(ticker, wait_time)
 
 
-def _rm_crdownload(stocks, downloads):
-    for ticker in stocks:
+def _rm_crdownload(tickers, downloads):
+    for ticker in tickers:
         path = downloads + _yahoo_ticker_format(ticker) + ".crdownload"
         if os.path.exists(path):
             os.remove(path)
@@ -70,9 +68,9 @@ def _exists(ticker, downloads):
     return os.path.exists(download_ticker_path(ticker, downloads))
 
 
-def _get_failed(stocks, downloads):
+def _get_failed(tickers, downloads):
     failed = []
-    for ticker in stocks:
+    for ticker in tickers:
         if not _exists(ticker, downloads):
             failed.append(ticker)
     return failed
@@ -83,19 +81,17 @@ WAIT_TIME_SLOW1 = 1
 WAIT_TIME_SLOW2 = 5
 
 
-def download_tickers(stocks, downloads):
+def download_tickers(tickers, downloads):
     driver = _Driver()
     print("DOWNLOAD TICKERS PART 1/3: ")
-    _download_tickers_control(driver, stocks, WAIT_TIME_FAST)
-    _rm_crdownload(stocks, downloads)
-    failed = _get_failed(stocks, downloads)
-    failed_stocks = {x: stocks[x] for x in failed}
+    _download_tickers_control(driver, tickers, WAIT_TIME_FAST)
+    _rm_crdownload(tickers, downloads)
+    failed = _get_failed(tickers, downloads)
     print("DOWNLOAD TICKERS PART 2/3: ")
-    _download_tickers_control(driver, failed_stocks, WAIT_TIME_SLOW1)
-    _rm_crdownload(stocks, downloads)
-    failed1 = _get_failed(failed_stocks, downloads)
-    failed1_stocks = {x: failed_stocks[x] for x in failed1}
+    _download_tickers_control(driver, failed, WAIT_TIME_SLOW1)
+    _rm_crdownload(failed, downloads)
+    failed1 = _get_failed(failed, downloads)
     print("DOWNLOAD TICKERS PART 3/3: ")
-    _download_tickers_control(driver, failed1_stocks, WAIT_TIME_SLOW2)
-    _rm_crdownload(stocks, downloads)
-    return _get_failed(failed1_stocks, downloads)
+    _download_tickers_control(driver, failed1, WAIT_TIME_SLOW2)
+    _rm_crdownload(failed1, downloads)
+    return _get_failed(failed1, downloads)
