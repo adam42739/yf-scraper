add34729f9a6ea02d42bf8a601c098dd87f10483
Adam Lynch <adamclynch03@gmail.com>
2024-09-07 00:40:07 -0400

    build: dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="yfscraper",
-    version="0.1.0",
+    version="0.2.0",
     description="Tool for scraping stock data from yahoo.com",
     url="https://github.com/adam42739/yf-scraper",
     author="Adam Lynch",
