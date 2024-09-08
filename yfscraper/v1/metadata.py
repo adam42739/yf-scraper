f9b5ce79a7429a246c8c6840a9632f0265ae96ed
Adam Lynch <adamclynch03@gmail.com>
2024-09-08 12:26:29 -0400

    fix: metadata saves a copy

--- yfscraper/v1/metadata.py
+++ yfscraper/v1/metadata.py
@@ -10,15 +10,24 @@ def get_metadata(base):
         with open(path, "r") as file:
             data = json.load(file)
     for ticker in data:
-        data[ticker]["start_date"] = datetime.datetime.strptime(data[ticker]["start_date"], "%Y%m%d")
-        data[ticker]["end_date"] = datetime.datetime.strptime(data[ticker]["end_date"], "%Y%m%d")
+        data[ticker]["start_date"] = datetime.datetime.strptime(
+            data[ticker]["start_date"], "%Y%m%d"
+        )
+        data[ticker]["end_date"] = datetime.datetime.strptime(
+            data[ticker]["end_date"], "%Y%m%d"
+        )
     return data
 
 
 def write_metadata(data, base):
+    data_write = {}
     for ticker in data:
-        data[ticker]["start_date"] = datetime.datetime.strftime(data[ticker]["start_date"], "%Y%m%d")
-        data[ticker]["end_date"] = datetime.datetime.strftime(data[ticker]["end_date"], "%Y%m%d")
+        data_write[ticker] = {
+            "start_date": datetime.datetime.strftime(
+                data[ticker]["start_date"], "%Y%m%d"
+            ),
+            "end_date": datetime.datetime.strftime(data[ticker]["end_date"], "%Y%m%d"),
+        }
     path = base + "_metadata.json"
     with open(path, "w") as file:
-        json.dump(data, file)
+        json.dump(data_write, file)
